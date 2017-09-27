35abed641f7f7f5715fe217fc787f9e8627f06a8
Michal Katanski <mike.katanski@gmail.com>
2017-09-27 11:38:53 +0200

    style(storybook): add sections to storybook stories

--- stories/CoreComponents.js
+++ stories/CoreComponents.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import Section from '../storybook-components/Section'
 import Playground from '../storybook-components/Playground'
 
-storiesOf('Core Components', module)
-  .add('Default', () => <Playground />)
+storiesOf('Higer Order Components', module)
+  .add('StrapInput', () => (
+    <Section
+      storyTitle="StrapInput"
+    >
+      <Playground />
+    </Section>
+  ))
