2de57052828d55c7b7e39791b327a96a60013f11
Michal Katanski <mike.katanski@gmail.com>
2017-10-19 19:21:01 +0200

    fix(tests): fix tests for StrapGroup and add some new

--- components/__tests__/StrapGroup-test.js
+++ components/__tests__/StrapGroup-test.js
@@ -33,15 +33,18 @@ describe('StrapGroup', () => {
 
     expect(component.state('errors')).toEqual({ })
     expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
 
     await instance.handleOnInputChange({
       inputName: 'testInput',
       errors: 'some errors',
       warnings: 'some warnings',
+      value: 'test value',
     })
 
     expect(component.state('errors')).toEqual({ testInput: 'some errors' })
     expect(component.state('warnings')).toEqual({ testInput: 'some warnings' })
+    expect(component.state('values')).toEqual({ testInput: 'test value' })
     expect(instance.isPristine).toBe(false)
     expect(instance.touched).toBe(false)
 
@@ -66,16 +69,20 @@ describe('StrapGroup', () => {
 
     expect(component.state('errors')).toEqual({ })
     expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
 
     await instance.handleOnInputChange({
       inputName: 'wrongInput',
       errors: 'some errors',
       warnings: 'some warnings',
+      value: 'test value',
     })
 
     expect(component.state('errors')).toEqual({ })
     expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
     expect(instance.isPristine).toBe(true)
+    expect(instance.touched).toBe(false)
 
     done()
   })
@@ -98,15 +105,18 @@ describe('StrapGroup', () => {
 
     expect(component.state('errors')).toEqual({ })
     expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
 
     await instance.handleOnInputBlur({
       inputName: 'testInput',
       errors: 'some errors',
       warnings: 'some warnings',
+      value: 'test value',
     })
 
     expect(component.state('errors')).toEqual({ testInput: 'some errors' })
     expect(component.state('warnings')).toEqual({ testInput: 'some warnings' })
+    expect(component.state('values')).toEqual({ testInput: 'test value' })
     expect(instance.isPristine).toBe(true)
     expect(instance.touched).toBe(true)
 
@@ -131,16 +141,215 @@ describe('StrapGroup', () => {
 
     expect(component.state('errors')).toEqual({ })
     expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
 
     await instance.handleOnInputBlur({
       inputName: 'wrongInput',
       errors: 'some errors',
       warnings: 'some warnings',
+      value: 'test value',
     })
 
     expect(component.state('errors')).toEqual({ })
     expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
     expect(instance.isPristine).toBe(true)
+    expect(instance.touched).toBe(false)
+
+    done()
+  })
+
+  it('handle handleOnFormUpdate when submitting', async (done) => {
+    const mockListenTo = jest.fn()
+    const mockDispatchEvent = jest.fn()
+    const component = shallow(
+      <Group />,
+      {
+        context: {
+          listenTo: mockListenTo,
+          dispatchEvent: mockDispatchEvent,
+        },
+      }
+    )
+    const instance = component.instance()
+
+    instance.inputNames = ['testInput']
+
+    expect(component.state('errors')).toEqual({ })
+    expect(component.state('warnings')).toEqual({ })
+
+    await instance.handleOnFormUpdate({
+      values: { testInput: 'submit values', wrongInput: 'wrong' },
+      errors: { testInput: 'submit errors', wrongInput: 'wrong' },
+      warnings: { testInput: 'submit warnings', wrongInput: 'wrong' },
+      isSubmitting: true,
+    })
+
+    expect(component.state('errors')).toEqual({ testInput: 'submit errors' })
+    expect(component.state('warnings')).toEqual({ testInput: 'submit warnings' })
+    expect(component.state('values')).toEqual({ testInput: 'submit values' })
+    expect(instance.isPristine).toBe(true)
+    expect(instance.touched).toBe(true)
+
+    done()
+  })
+
+  it('handle handleOnFormUpdate not submitting', async (done) => {
+    const mockListenTo = jest.fn()
+    const mockDispatchEvent = jest.fn()
+    const component = shallow(
+      <Group />,
+      {
+        context: {
+          listenTo: mockListenTo,
+          dispatchEvent: mockDispatchEvent,
+        },
+      }
+    )
+    const instance = component.instance()
+
+    instance.inputNames = ['testInput']
+
+    expect(component.state('errors')).toEqual({ })
+    expect(component.state('warnings')).toEqual({ })
+
+    await instance.handleOnFormUpdate({
+      values: { testInput: 'submit values' },
+      errors: { testInput: 'submit errors' },
+      warnings: { testInput: 'submit warnings' },
+      isSubmitting: false,
+    })
+
+    expect(component.state('errors')).toEqual({ })
+    expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
+    expect(instance.isPristine).toBe(true)
+    expect(instance.touched).toBe(false)
+
+    done()
+  })
+
+  it('handle registerToGroup correct', async (done) => {
+    const mockListenTo = jest.fn()
+    const mockDispatchEvent = jest.fn()
+    const component = shallow(
+      <Group />,
+      {
+        context: {
+          listenTo: mockListenTo,
+          dispatchEvent: mockDispatchEvent,
+        },
+      }
+    )
+    const instance = component.instance()
+
+    instance.inputNames = []
+
+    expect(instance.inputNames).toEqual([])
+
+    instance.handleRegisterToGroup('someInput')
+    instance.handleRegisterToGroup('someInput')
+    instance.handleRegisterToGroup('anotherInput')
+
+    expect(instance.inputNames).toEqual(['someInput', 'anotherInput'])
+
+    done()
+  })
+
+  it('handle deregisterFromGroup correct', async (done) => {
+    const mockListenTo = jest.fn()
+    const mockDispatchEvent = jest.fn()
+    const component = shallow(
+      <Group />,
+      {
+        context: {
+          listenTo: mockListenTo,
+          dispatchEvent: mockDispatchEvent,
+        },
+      }
+    )
+    const instance = component.instance()
+
+    instance.inputNames = ['firstInput', 'secondInput', 'thirdInput']
+
+    expect(instance.inputNames).toEqual(['firstInput', 'secondInput', 'thirdInput'])
+
+    instance.handleDeregisterFromGroup('secondInput')
+    instance.handleDeregisterFromGroup('secondInput')
+
+    expect(instance.inputNames).toEqual(['firstInput', 'thirdInput'])
+
+    done()
+  })
+
+  it('handle handleOnAfterSyncValidation successfull', async (done) => {
+    const mockListenTo = jest.fn()
+    const mockDispatchEvent = jest.fn()
+    const component = shallow(
+      <Group />,
+      {
+        context: {
+          listenTo: mockListenTo,
+          dispatchEvent: mockDispatchEvent,
+        },
+      }
+    )
+    const instance = component.instance()
+
+    instance.inputNames = ['testInput']
+
+    expect(component.state('errors')).toEqual({ })
+    expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
+
+    instance.handleOnAfterSyncValidation({
+      inputName: 'testInput',
+      errors: 'some errors',
+      warnings: 'some warnings',
+      value: 'test value',
+    })
+
+    expect(component.state('errors')).toEqual({ testInput: 'some errors' })
+    expect(component.state('warnings')).toEqual({ testInput: 'some warnings' })
+    expect(component.state('values')).toEqual({ testInput: 'test value' })
+    expect(instance.isPristine).toBe(true)
+    expect(instance.touched).toBe(false)
+
+    done()
+  })
+
+  it('handle handleOnAfterSyncValidation unsuccessfull', async (done) => {
+    const mockListenTo = jest.fn()
+    const mockDispatchEvent = jest.fn()
+    const component = shallow(
+      <Group />,
+      {
+        context: {
+          listenTo: mockListenTo,
+          dispatchEvent: mockDispatchEvent,
+        },
+      }
+    )
+    const instance = component.instance()
+
+    instance.inputNames = ['testInput']
+
+    expect(component.state('errors')).toEqual({ })
+    expect(component.state('warnings')).toEqual({ })
+    expect(component.state('values')).toEqual({ })
+
+    instance.handleOnAfterSyncValidation({
+      inputName: 'wrongInput',
+      errors: 'some errors',
+      warnings: 'some warnings',
+      value: 'test value',
+    })
+
+    expect(component.state('errors')).toEqual({})
+    expect(component.state('warnings')).toEqual({})
+    expect(component.state('values')).toEqual({})
+    expect(instance.isPristine).toBe(true)
+    expect(instance.touched).toBe(false)
 
     done()
   })
