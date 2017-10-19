50c689ae54c545dc2954887a043810af754ac3d8
Michal Katanski <mike.katanski@gmail.com>
2017-10-19 18:40:39 +0200

    Create xamples folder

--- stories/CoreComponents.js
+++ stories/CoreComponents.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import Section from '../storybook-components/Section'
-import MyForm from '../storybook-components/basic-usage-guide/custom/MyForm'
-import BasicForm from '../storybook-components/reactstrap/basic-form/ReactstrapForm'
+import Section from '../examples/Section'
+import MyForm from '../examples/basic-usage-guide/custom/MyForm'
+import BasicForm from '../examples/reactstrap/basic-form/ReactstrapForm'
 
 storiesOf('Basic Usage Guide', module)
   .add('Creating simple form using custom components', () => (
