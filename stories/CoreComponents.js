22e3cff24048ac521442ea83e43e44a73b4d5fe8
Michal Katanski <mike.katanski@gmail.com>
2017-11-14 00:32:12 +0100

    Remove section component from examples

--- stories/CoreComponents.js
+++ stories/CoreComponents.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import Section from '../examples/Section'
 import MyForm from '../examples/basic-usage-guide/MyForm'
 import BasicForm from '../examples/reactstrap/basic-form'
 import Programmatic from '../examples/reactstrap/programmatic-submission'
@@ -9,32 +8,16 @@ import SetValue from '../examples/reactstrap/set-value'
 
 storiesOf('Basic Usage Guide', module)
   .add('Creating simple form using custom components', () => (
-    <Section
-      storyTitle="Creating simple form using custom components"
-    >
-      <MyForm />
-    </Section>
+    <MyForm />
   ))
 
 storiesOf('Examples with Reactstrap', module)
   .add('Basic Form', () => (
-    <Section
-      storyTitle="Basic Form"
-    >
-      <BasicForm />
-    </Section>
+    <BasicForm />
   ))
   .add('Programmatic submission and validation', () => (
-    <Section
-      storyTitle="Programmatic submission and validation"
-    >
-      <Programmatic />
-    </Section>
+    <Programmatic />
   ))
   .add('Set value for input', () => (
-    <Section
-      storyTitle="Set value for input"
-    >
-      <SetValue />
-    </Section>
+    <SetValue />
   ))
